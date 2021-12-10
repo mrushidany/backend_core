d3539e1cce3859d75d7fbe42dcca9f3491f788a0
mrushidany <mrushidany@gmail.com>
2021-12-10 14:25:49 +0300

    Defined the validity of access tokens to 1 day

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
 
@@ -31,6 +32,7 @@ class AuthServiceProvider extends ServiceProvider
             Passport::routes();
         }
 
-        //
+        //Defining the validity of access tokens
+        Passport::tokensExpireIn(Carbon::now()->addDays(1));
     }
 }
